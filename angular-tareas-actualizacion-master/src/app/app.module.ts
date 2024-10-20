bbc2401d395e74f91efe1d5e22cf0983369fcfda
Valeria <valeriamarilef21@gmail.com>
2024-10-20 20:36:46 -0300

    mensaje

---
+++ angular-tareas-actualizacion-master/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+// Componentes
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TareasComponent } from './components/tareas/tareas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    TareasComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
